f030e47c566e1bf0c2565a40cb2b7bd9a382773c
图你怀中安稳 <kongpingan@kerust.com>
2018-09-07 14:41:17 +0800

    Initial commit

---
+++ src/main/java/com/kpa/NoNameProject/NoNameProjectApplication.java
@@ -0,0 +1,12 @@
+package com.kpa.NoNameProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoNameProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoNameProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kpa/NoNameProject/NoNameProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kpa.NoNameProject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NoNameProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
